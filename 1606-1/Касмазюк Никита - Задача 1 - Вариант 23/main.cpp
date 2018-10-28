c76d54c74dcb9be1550ea383a7647a1052b44639
Kasmazyuk <Kasmazyuk@github.com>
2018-10-28 17:04:12 +0300

    28.10

--- 1606-1/Касмазюк Никита - Задача 1 - Вариант 23/main.cpp
+++ 1606-1/Касмазюк Никита - Задача 1 - Вариант 23/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <ctime>
 #include <cmath>
+#include <sstream>
 
 using namespace std;
 
@@ -11,20 +12,32 @@ int main (int argc, char **argv)
 {
 	srand(time(0));
 
-	int n = 10;
+	int n;
 	int borderL, borderR;
-	char s = 'E';
+	char s;
 	char *str;
+	char *rbuf;
 	int par_count = 0;
 	int count = 0;
 	double timest, timefin;
 
+	stringstream convert(argv[1]); 
+	stringstream convert1(argv[2]);
+
+	if (!(convert >> n)) 
+		n = 0; 
+
+	if (!(convert1 >> s)) 
+		s = '0'; 
+
+
 	str = new char [n];
-	for (int i = 0; i < n; i++)
-	{
-		str[i] = rand()%10 + 65;
-	  //printf("%c", str[i]);
-	}
+	rbuf = new char [n];
+	//for (int i = 0; i < n; i++)
+	//{
+	//	str[i] = rand()%10 + 65;
+	//  //printf("%c", str[i]);
+	//}
 
 	int ProcNum, ProcRank;
 	MPI_Init(&argc, &argv);
@@ -32,22 +45,38 @@ int main (int argc, char **argv)
 	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
 	MPI_Status Status;
 
+	int g = n % ProcNum;
 	int k = n / ProcNum;
+	k = k + g;
 	int proc_count = 0;
 
-	MPI_Barrier(MPI_COMM_WORLD);
+	//MPI_Barrier(MPI_COMM_WORLD);
 
 	if (ProcRank == 0) 
+	{
+			/*str = new char [n];*/
+			for (int i = 0; i < n; i++)
+			{
+				str[i] = rand()%10 + 65;
+			  printf("%c ", str[i]);
+	  
+			}
+			  printf("\n");
+		/*for (int count=0; count < argc; ++count)
+        std::cout << count << " " << argv[count] << '\n';*/
+
 		timest = MPI_Wtime();
+	}
 
-	MPI_Bcast(str, n, MPI_CHAR, 0, MPI_COMM_WORLD);
+	MPI_Scatter(str, k, MPI_CHAR, rbuf, k, MPI_CHAR, 0, MPI_COMM_WORLD);
+	//MPI_Bcast(str, n, MPI_CHAR, 0, MPI_COMM_WORLD);
 
-	borderL = (int)(k*ProcRank);
-	borderR = (int)(k*(ProcRank + 1));
-	if (ProcRank == ProcNum - 1) 
-		borderR = n;
-	for (int j = borderL; j < borderR; j++)
-		if (str[j] == s)
+	//borderL = (int)(k*ProcRank);
+	//borderR = (int)(k*(ProcRank + 1));
+	//if (ProcRank == ProcNum - 1) 
+	//	borderR = n;
+	for (int j = 0; j < k; j++)
+		if (rbuf[j] == s)
 			proc_count++;
 	MPI_Reduce(&proc_count, &par_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
@@ -56,7 +85,7 @@ int main (int argc, char **argv)
 		timefin = MPI_Wtime();
 		float res = (float)par_count/(float)n;
 		printf("Repeatabillity = %f\n", res);
-		printf("Time work = %f", timefin - timest);
+		printf("Time work      = %f", timefin - timest);
 
 	}
 
