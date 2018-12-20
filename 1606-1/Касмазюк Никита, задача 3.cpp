da6c9866938202de508bd0b550f58bcae6272d0e
Kasmazyuk <32219778+Kasmazyuk@users.noreply.github.com>
2018-12-20 21:59:20 +0300

    Update Касмазюк Никита, задача 3.cpp

--- 1606-1/Касмазюк Никита, задача 3.cpp
+++ 1606-1/Касмазюк Никита, задача 3.cpp
@@ -6,6 +6,11 @@
 
 using namespace std;
 
+double* CreateVectorMatrix(int N){
+	double *matrix = new double[N*N];
+
+	return matrix;
+}
 
 void PrintMatrix (double* matrix,int N){
 	for (int i = 0; i < N; i++){
@@ -36,7 +41,7 @@ void RandMatrix(double* matrix1, double* matrix2, int N){
 }
 
 double* Trivial_alghorithm(double* matrix1, double* matrix2, int N){
-	double* Rez = new double[N*N];
+	double* Rez = CreateVectorMatrix(N);
 	double sum;
 	for(int i = 0; i < N; i++)
 		for(int j = 0; j < N; j++){
@@ -49,7 +54,7 @@ double* Trivial_alghorithm(double* matrix1, double* matrix2, int N){
 }
 
 double* Add(double* matrix1, double* matrix2, int N){
-	double* Rez = new double[N*N];
+	double* Rez = CreateVectorMatrix(N);
 	
 	for (int i = 0; i < N; i++)
 		for(int j = 0; j < N; j++)
@@ -59,7 +64,7 @@ double* Add(double* matrix1, double* matrix2, int N){
 }
 
 double* Add(double* matrix1, double* matrix2, double* matrix3, double* matrix4, int N){
-	double* Rez = new double[N*N];
+	double* Rez = CreateVectorMatrix(N);
 	
 	for (int i = 0; i < N; i++)
 		for(int j = 0; j < N; j++)
@@ -69,7 +74,7 @@ double* Add(double* matrix1, double* matrix2, double* matrix3, double* matrix4, 
 }
 
 double* Sub(double* matrix1, double* matrix2, int N){
-	double* Rez = new double[N*N];
+	double* Rez = CreateVectorMatrix(N);
 	
 	for (int i = 0; i < N; i++)
 		for(int j = 0; j < N; j++)
@@ -78,8 +83,8 @@ double* Sub(double* matrix1, double* matrix2, int N){
 	return Rez;
 }
 
-double* Sub(double* matrix1, double* matrix2, double* matrix3, double* matrix4, int N){//ñóììà 3-õ ìàòðèö âû÷åñòü ÷åòâåðòóþ
-	double* Rez = new double[N*N];
+double* Sub(double* matrix1, double* matrix2, double* matrix3, double* matrix4, int N){
+	double* Rez = CreateVectorMatrix(N);
 
 	for (int i = 0; i < N; i++)
 		for(int j = 0; j < N; j++)
@@ -88,27 +93,27 @@ double* Sub(double* matrix1, double* matrix2, double* matrix3, double* matrix4, 
 	return Rez;
 }
 
-double* alg(double* matrix1, double* matrix2, int N, int threshold){
+double* Shtr_alg(double* matrix1, double* matrix2, int N, int threshold){
 	double* Rez;
 
 	if(N <= threshold){
 		Rez = Trivial_alghorithm(matrix1, matrix2, N);
 	}
 	else{
-		Rez = new double[N*N];
+		Rez = CreateVectorMatrix(N);
 		N = N/2;
 		double* A[4];
 		double* B[4];
 		double* C[4];
 		double* P[7];
 
-
+	
 		for(int i = 0; i < 4; i++){
-			A[i] = new double[N*N];
-			B[i] = new double[N*N];
+			A[i] = CreateVectorMatrix(N);
+			B[i] = CreateVectorMatrix(N);
 		}
 
-
+		
 		for(int i = 0; i < N; i++)
 			for(int j = 0; j < N; j++){
 				int index_new = i*N+j,index_old = 2*i*N+j, N_N = 2*N*N;
@@ -123,47 +128,48 @@ double* alg(double* matrix1, double* matrix2, int N, int threshold){
 				B[3][index_new] = matrix2[index_old+N_N+N];
 			}
 
+		
 		double *TMP = Add(A[0], A[3], N);
 		double *_TMP = Add(B[0], B[3], N);
-		P[0] = alg(TMP, _TMP, N,threshold);
+		P[0] = Shtr_alg(TMP, _TMP, N,threshold);
 		delete[] TMP;
 		delete[] _TMP;
 
 		TMP = Add(A[2], A[3], N);
-		P[1] = alg(TMP, B[0], N, threshold);
+		P[1] = Shtr_alg(TMP, B[0], N, threshold);
 		delete[] TMP;
 
 		TMP = Sub(B[1], B[3], N);
-		P[2] = alg(A[0], TMP, N, threshold);
+		P[2] = Shtr_alg(A[0], TMP, N, threshold);
 		delete[] TMP;
 
 		TMP = Sub(B[2], B[0], N);
-		P[3] = alg(A[3], TMP, N, threshold);
+		P[3] = Shtr_alg(A[3], TMP, N, threshold);
 		delete[] TMP;
 
 		TMP = Add(A[0], A[1], N);
-		P[4] = alg(TMP, B[3], N, threshold);
+		P[4] = Shtr_alg(TMP, B[3], N, threshold);
 		delete[] TMP;
 
 		TMP = Sub(A[2], A[0], N);
 		_TMP = Add(B[0], B[1], N);
-		P[5] = alg(TMP, _TMP, N, threshold);
+		P[5] = Shtr_alg(TMP, _TMP, N, threshold);
 		delete[] TMP;
 		delete[] _TMP;
 
 		TMP = Sub(A[1], A[3], N);
 		_TMP = Add(B[2], B[3], N);
-		P[6] = alg(TMP, _TMP, N, threshold);
+		P[6] = Shtr_alg(TMP, _TMP, N, threshold);
 		delete[] TMP;
 		delete[] _TMP;
 
-
+		
 		C[0] = Sub(P[0], P[3], P[6], P[4], N);
 		C[1] = Add(P[2], P[4], N);
 		C[2] = Add(P[1], P[3], N);
 		C[3] = Sub(P[0], P[2], P[5], P[1], N);
 
-
+	
 		for(int i = 0; i < N; i++)
 			for(int j = 0; j < N; j++){
 					Rez[i*2*N+j] = C[0][i*N+j];
@@ -172,6 +178,7 @@ double* alg(double* matrix1, double* matrix2, int N, int threshold){
 					Rez[i*2*N+j+2*N*N+N] = C[3][i*N+j];
 			}
 
+		
 		for(int i = 0; i < 4; i++){
 			delete[] A[i];
 			delete[] B[i];
@@ -186,8 +193,7 @@ double* alg(double* matrix1, double* matrix2, int N, int threshold){
 }
 
 int main(int argc, char** argv){
-	double *matr_A, *matr_B, *matr_Rez_Seq, *matr_Rez_Par, 
-		**A, **B, **TMP_Rez;
+	double *matr_A, *matr_B, *matr_Rez_Seq, *matr_Rez_Par, **A, **B, **TMP_Rez;
 	int  N, thr = 64, sqr, new_N;
 
 	int ProcSize = 1, ProcRank = 0;
@@ -200,12 +206,18 @@ int main(int argc, char** argv){
 	double TimeSeqAlg = 0;
 	double TimeParAlg = 0;
 
+
+	// ----------------------- //
+
+
+	 setlocale(LC_ALL, "Russian");
+
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &ProcSize);
 	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
 
 	if (ProcRank == 0){
-		
+	
 		int k = 0;
 		if (argc > 1)
 			k = atoi (argv[1]);
@@ -213,31 +225,40 @@ int main(int argc, char** argv){
 		if(argc > 2)
 			thr = atoi(argv[2]);
 
-		
-		matr_A= new double[N*N];
-		matr_B= new double[N*N];
-		matr_Rez_Par= new double[N*N];
+	
+		matr_A=CreateVectorMatrix(N);
+		matr_B=CreateVectorMatrix(N);
+		matr_Rez_Par=CreateVectorMatrix(N);
 		RandMatrix(matr_A, matr_B, N);
 
 
-		if (N < 20){
+		char fA[20] = "D:\MatrA.txt";
+		char fB[20] = "D:\MatrB.txt";
+
+		if (N < 10){
 			cout << "Matr A:" << endl;
 			PrintMatrix(matr_A, N);
 			cout << "Matr B:" << endl;
 			PrintMatrix(matr_B, N);
 		}
-	
+		else{
+			fPrintMatrix(matr_A, N, fA);
+			fPrintMatrix(matr_B, N, fB);
+		}
 
-			// àëãîðèòì
+		// --------------------- // 
+
+
 		StartParAlg = MPI_Wtime();
+
 
 		sqr = (int)sqrt((double)ProcSize), new_N = N/sqr;
 		A = new double*[ProcSize], B = new double*[ProcSize];
 	
 
 		for(int i = 0; i < ProcSize; i++){
-			A[i] = new double[N*N];
-			B[i] = new double[N*N];
+			A[i] = CreateVectorMatrix(new_N);
+			B[i] = CreateVectorMatrix(new_N);
 		}
 
 		for(int i = 0; i < N; i++)
@@ -245,6 +266,7 @@ int main(int argc, char** argv){
 				A[sqr*(i/new_N)+j/new_N][(i%new_N)*new_N+(j%new_N)] = matr_A[i*N+j];
 				B[sqr*(i/new_N)+j/new_N][(i%new_N)*new_N+(j%new_N)] = matr_B[i*N+j];
 			}
+
 
 		MPI_Bcast(&N, 1,  MPI_INT, 0, MPI_COMM_WORLD); 
 		MPI_Bcast(&thr, 1,  MPI_INT, 0, MPI_COMM_WORLD);
@@ -259,12 +281,14 @@ int main(int argc, char** argv){
 			}
 		}
 
+		
 		for(int i = 0; i < sqr; i++){
 			double* TMP = B[i];
 			B[i] = B[i*sqr];
 			B[i*sqr] = TMP;
 		}
 	}
+
 
 	if(ProcRank !=0){
 		MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD); 
@@ -274,8 +298,8 @@ int main(int argc, char** argv){
 
 		A = new double*[sqr], B = new double*[sqr];
 		for(int i = 0; i < sqr; i++){
-			A[i] = new double[new_N*new_N];
-			B[i] = new double[new_N*new_N];
+			A[i] = CreateVectorMatrix(new_N);
+			B[i] = CreateVectorMatrix(new_N);
 		}
 
 		for(int i = 0; i < sqr; i++){
@@ -284,14 +308,18 @@ int main(int argc, char** argv){
 		}
 	}
 
+
 	TMP_Rez = new double*[sqr+1];
 	for(int i = 0; i < sqr; i++){
-		TMP_Rez[i+1] = alg(A[i], B[i], new_N, thr);
+		TMP_Rez[i+1] = Shtr_alg(A[i], B[i], new_N, thr);
 	}
 	if(ProcSize == 4)
 		TMP_Rez[0] = Add(TMP_Rez[1], TMP_Rez[2], new_N);
 	if(ProcSize == 16)
 		TMP_Rez[0] = Add(TMP_Rez[1], TMP_Rez[2], TMP_Rez[3], TMP_Rez[4], new_N);
+	
+
+
 
 	if(ProcRank == 0){
 		for(int i = 0; i < ProcSize; i++){
@@ -311,32 +339,35 @@ int main(int argc, char** argv){
 	delete[] A;
 	delete[] B;
 
+
+
 	if (ProcRank != 0)	{
 		MPI_Send(TMP_Rez[0], new_N*new_N, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
 		delete[] TMP_Rez[0];
 	}
+
 	
 	if (ProcRank == 0){
 		int coef = sqrt(ProcSize);
-
 		for(int i = 0; i < new_N; i++)
 			for(int j = 0; j < new_N; j++)
 				matr_Rez_Par[coef*i*new_N+j] = TMP_Rez[0][i*new_N+j];
 		for(int k = 1; k < ProcSize; k++){
-
 			MPI_Recv(TMP_Rez[0], new_N*new_N, MPI_DOUBLE, k, 0, MPI_COMM_WORLD, &Status);
 			for(int i = 0; i < new_N; i++)
 				for(int j = 0; j < new_N; j++)
 					matr_Rez_Par[(k/coef)*new_N*N+(k%coef)*new_N+coef*i*new_N+j] = TMP_Rez[0][i*new_N+j];
 		}
 
-		//âûâîäû
+		// ----------------- //
+
 
 		EndParAlg = MPI_Wtime();
 		TimeParAlg = EndParAlg - StartParAlg;
 		
-		cout << "ParAlg time = " << TimeParAlg << endl;
-		char fParAlg[20] = "ParAlg.txt";
+		
+		cout << "Par. Algoritm time = " << TimeParAlg << endl;
+		char fParAlg[20] = "D:\ParAlg.txt";
 			if (N < 20){
 				cout << "Matr C:" << endl;
 				PrintMatrix(matr_Rez_Par, N);
@@ -347,14 +378,16 @@ int main(int argc, char** argv){
 
 		delete[] TMP_Rez[0];
 
+		
 		StartSeqAlg = MPI_Wtime();
-		matr_Rez_Seq = alg(matr_A, matr_B, N, thr);
+		matr_Rez_Seq = Shtr_alg(matr_A, matr_B, N, thr);
 		EndSeqAlg = MPI_Wtime();
 		TimeSeqAlg = EndSeqAlg - StartSeqAlg;
 		
-		cout << "SeqAlg time = " << TimeSeqAlg << endl;
-		char fSeqAlg[20] = "SeqAlg.txt";
-		if (N < 20){
+
+		cout << "Posl. Algoritm time = " << TimeSeqAlg << endl;
+		char fSeqAlg[20] = "D:\SeqAlg.txt";
+		if (N < 10){
 			cout << "Matr C:" << endl;
 			PrintMatrix(matr_Rez_Seq, N);
 		}
@@ -362,17 +395,24 @@ int main(int argc, char** argv){
 			fPrintMatrix(matr_Rez_Seq, N, fSeqAlg);
 		}
 
-		//bool flag = false;
-		//for(int k = 0; k < N; k++)
-		//	for(int l = 0; l < N; l++)
-		//		if(matr_Rez_Seq[k*N+l] != matr_Rez_Par[k*N+l]) 
-		//			flag = true;
+		
+		
+			cout<< "Speed up = " << TimeSeqAlg/TimeParAlg  << endl;
+		
+			
+
+		bool flag = false;
+		for(int k = 0; k < N; k++)
+			for(int l = 0; l < N; l++)
+				
+				if(ceil (matr_Rez_Seq[k*N+l]) != ceil  (matr_Rez_Par[k*N+l])) 
+					flag = true;
 
 
-		//if(flag)
-		//	cout << "matr_Rez_Seq != matr_Rez_Par" << endl;
-		//if(!flag)
-		//	cout << "matr_Rez_Seq == matr_Rez_Par" << endl;
+		if(flag)
+			cout << "result posl. != result par." << endl;
+		if(!flag)
+			cout << "result posl. == result par." << endl;
 		delete[] matr_Rez_Par;
 		delete[] matr_Rez_Seq;
 		delete[] matr_A;
